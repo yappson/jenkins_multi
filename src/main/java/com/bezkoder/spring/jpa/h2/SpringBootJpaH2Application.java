a6da5975f98485963ba06425bcafc5da2a0440dd
yatendra.singh <11352997+yappson@users.noreply.github.com>
2023-12-20 16:37:33 +0530

    first

---
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -0,0 +1,19 @@
+package com.bezkoder.spring.jpa.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaH2Application.class, args);
+	}
+
+}
+
+//kjsdnfkjdsnkjfskjdnbfkjsbdjdbjhasbdjhasbj
+//dscnksdnjck
+//final test
